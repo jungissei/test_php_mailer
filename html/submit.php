06fa972f9bcdea9dccdda9937c9e813fcf2a4f7d
jungiIssei <jungissei@gmail.com>
2022-10-04 21:34:39 +0900

    add Valitron

--- html/submit.php
+++ html/submit.php
@@ -17,31 +17,50 @@ use PHPMailer\PHPMailer\Exception;
 // Composer の autoloader をロード
 require 'vendor/autoload.php';
 
-// インスタンス生成
-$mail = new PHPMailer(true);
 
-// .envを使用する
-Dotenv\Dotenv::createImmutable(__DIR__)->load();
+Valitron\Validator::lang('ja');
 
-try {
-    // SMTPの設定
-    $mail->isSMTP();                       // SMTP 利用
-    $mail->Host       = $_ENV['MAIL_HOST'];  // SMTP サーバー(Gmail の場合これ)
-    $mail->SMTPAuth   = true;              // SMTP認証を有効にする
-    $mail->Username   = $_ENV['MAIL_USERUNAME']; // ユーザ名 (Gmail ならメールアドレス)
-    $mail->Password   = $_ENV['MAIL_PASSWORD'];      // パスワード
-    $mail->SMTPSecure = 'tls';             // 暗号化通信 (Gmail では使えます)
-    $mail->Port       = $_ENV['MAIL_PORT'];               // TCP ポート (TLS の場合 587)
+$v = new Valitron\Validator($_POST); // 検証する値を引数にインスタンスを生成します
 
-    // メール本体
-    $mail->setFrom($_ENV['MAIL_ADDRESS'], 'user');  // 送信元メールアドレスと名前
-    $mail->addAddress($to, mb_encode_mimeheader($name, 'ISO-2022-JP'));  // 送信先メールアドレスと名前
-    $mail->Subject = mb_encode_mimeheader($subject, 'ISO-2022-JP');  // 件名
-    $mail->Body    = mb_convert_encoding($message, "JIS","UTF-8");  // 本文
+$v->labels([
+  'name' => '名前',
+  'email' => 'メールアドレス'
+]);
 
-// 送信
-    $mail->send();
-    echo '送信済み';
-} catch (Exception $e) {
-    echo "送信失敗: {$mail->ErrorInfo}";
+$v->rule('required', ['name', 'email']); // ルールを設定します
+$v->rule('email', 'email');
+
+if($v->validate()) { // 検証
+  // インスタンス生成
+  $mail = new PHPMailer(true);
+
+  // .envを使用する
+  Dotenv\Dotenv::createImmutable(__DIR__)->load();
+
+  try {
+      // SMTPの設定
+      $mail->isSMTP();                       // SMTP 利用
+      $mail->Host       = $_ENV['MAIL_HOST'];  // SMTP サーバー(Gmail の場合これ)
+      $mail->SMTPAuth   = true;              // SMTP認証を有効にする
+      $mail->Username   = $_ENV['MAIL_USERUNAME']; // ユーザ名 (Gmail ならメールアドレス)
+      $mail->Password   = $_ENV['MAIL_PASSWORD'];      // パスワード
+      $mail->SMTPSecure = 'tls';             // 暗号化通信 (Gmail では使えます)
+      $mail->Port       = $_ENV['MAIL_PORT'];               // TCP ポート (TLS の場合 587)
+
+      // メール本体
+      $mail->setFrom($_ENV['MAIL_ADDRESS'], 'user');  // 送信元メールアドレスと名前
+      $mail->addAddress($to, mb_encode_mimeheader($name, 'ISO-2022-JP'));  // 送信先メールアドレスと名前
+      $mail->Subject = mb_encode_mimeheader($subject, 'ISO-2022-JP');  // 件名
+      $mail->Body    = mb_convert_encoding($message, "JIS","UTF-8");  // 本文
+
+  // 送信
+      $mail->send();
+      echo '送信済み';
+  } catch (Exception $e) {
+      echo "送信失敗: {$mail->ErrorInfo}";
+  }
+
+} else {
+  // Errors
+  print_r($v->errors());
 }
